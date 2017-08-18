a0748c680aaadd5fc184c84f0503c9be55cf6736
Holger Vetter <h.vetter@outlook.com>
2017-08-18 16:26:08 +0200

    replaced bat files with powershell (install/uninstall)

---
+++ Hosts/WindowsServiceHost/WindowsServiceHost/WindowsServiceHost/Scripts/InstallService.ps1
@@ -0,0 +1,8 @@
+﻿$ServiceName = Read-Host -Prompt 'Input the name of this service'
+$ServiceDescription = Read-Host -Prompt 'Input the description of this service'
+$Path = Read-Host -Prompt 'Input path of your service'
+
+$ServiceName = "Dtf.Service." + $ServiceName
+
+sc.exe create $ServiceName binPath= $Path start= auto
+sc.exe description $ServiceName $ServiceDescription
---
+++ Hosts/WindowsServiceHost/WindowsServiceHost/WindowsServiceHost/Scripts/UninstallService.ps1
@@ -0,0 +1,5 @@
+﻿$ServiceName = Read-Host -Prompt 'Name of Service to uninstall'
+
+$ServiceName = "Dtf.Service." + $ServiceName
+
+sc.exe delete $ServiceName
