2a3d9f4279996c577e9b6901b75cac8e8ca6fc36
hvetter <hvetter@daenet.de>
2017-10-19 17:33:23 +0200

    create unittest project for common

---
+++ Daenet.DurableTaskMicroservices.Common/Daenet.Microservice.Common.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Daenet.Microservice.Common.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
